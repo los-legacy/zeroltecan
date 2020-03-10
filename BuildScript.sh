ba413a17d52c97bacb6e2b3d1c3521b9ce732181
BenLue <BenJule@users.noreply.github.com>
2020-03-10 03:31:00 +0100

    Delete BuildScript.sh

--- BuildScript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/benlue/android/lineage
-export PATH=~/bin:$PATH
-export USE_CCACHE=1
-ccache -M 50G
-export CCACHE_COMPRESS=1
-#repo sync --no-clone-bundle --force-sync
-make clean;source build/envsetup.sh;breakfast zeroltecan;brunch zeroltecan
