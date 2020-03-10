d941e9e5a11d90023a2d7342fcc42541bdb105df
BenLue <BenJule@users.noreply.github.com>
2020-03-10 02:39:39 +0100

    Update BuildScript.sh

--- BuildScript.sh
+++ BuildScript.sh
@@ -6,8 +6,4 @@ export USE_CCACHE=1
 ccache -M 50G
 export CCACHE_COMPRESS=1
 #repo sync --no-clone-bundle --force-sync
-make clean
-ls -lah
-source build/envsetup.sh
-breakfast zeroltecan
-brunch zeroltecan
+make clean;source build/envsetup.sh;breakfast zeroltecan;brunch zeroltecan
