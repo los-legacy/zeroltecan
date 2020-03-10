ffab4853caa66a7302d749fbe706e3c7ec902b4e
BenLue <BenJule@users.noreply.github.com>
2020-03-10 02:42:05 +0100

    Update BuildScript.sh

--- BuildScript.sh
+++ BuildScript.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -x
 cd /home/benlue/android/lineage
 export PATH=~/bin:$PATH
 export USE_CCACHE=1
 ccache -M 50G
 export CCACHE_COMPRESS=1
 #repo sync --no-clone-bundle --force-sync
-make clean;./envsetup.sh;breakfast zeroltecan;brunch zeroltecan
+make clean;source build/envsetup.sh;breakfast zeroltecan;brunch zeroltecan
