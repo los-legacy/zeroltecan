d60596890f807c4ba33a93e44e98e4f3d7706c2d
BenLue <BenJule@users.noreply.github.com>
2020-03-10 01:07:57 +0100

    Create BuildScript.sh

---
+++ BuildScript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+cd /home/benlue/android/lineage
+export PATH=~/bin:$PATH
+export USE_CCACHE=1
+ccache -M 50G
+export CCACHE_COMPRESS=1
+#repo sync --no-clone-bundle --force-sync
+make clean
+source build/envsetup.sh && breakfast zeroltecan && time brunch zeroltecan
